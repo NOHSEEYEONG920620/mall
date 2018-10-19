a5628484a19e1046341d290df2eb5b6c5889610e
Administrator <Administrator@teacher202>
2018-10-19 15:07:30 +0900

    [박성환] Member관련 클래스 생성
    1. AddMember Controller생성
    2. MemberDao생성
    2-1. MemberDao.insertMember메서드 추가
    3. checkList파일 수정

--- mall/src/main/java/cafe/jjdev/mall/controller/IndexController.java
+++ mall/src/main/java/cafe/jjdev/mall/controller/IndexController.java
@@ -1,11 +1,15 @@
 package cafe.jjdev.mall.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import cafe.jjdev.mall.service.Member;
 
 @WebServlet({"/","/index"})
 public class IndexController extends HttpServlet {
@@ -14,12 +18,18 @@ public class IndexController extends HttpServlet {
 		// 1. router : @WebServlet("/index")
 		
 		// 2. model호출 : DAO모델생성
-		String model = "jjdev";
+		String model = "jjdev"; 
 		// view rend...(template)
 		
-		request.setAttribute("model", model);
-		// 4. Forward(request, response) to WEB-INF/jsp/index.jsp
+		request.setAttribute("model", model); // ${model}
+		Member member = new Member();
+		member.setNo(1); member.setId("guest"); member.setLevel(0);
+		request.setAttribute("member", member); // ${member.id}
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("승정"); list.add("승우"); list.add("찬우");
+		request.setAttribute("list", list); // ${list}
 		
+		// 4. Forward(request, response) to WEB-INF/jsp/index.jsp
 		request.getRequestDispatcher("WEB-INF/jsp/index.jsp")
 			.forward(request,response);
 	}
--- mall/src/main/webapp/WEB-INF/jsp/index.jsp
+++ mall/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page isELIgnored="false"%>
 <!DOCTYPE html>
 <html>
@@ -7,8 +8,20 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<%-- Hello <%=//request.getAttribute("model") %> --%> 
+	<%-- Hello <%=request.getAttribute("model") %> --%> 
 	<br>
 	Hello ${model}
+	<br>
+	<!--  if(){} -->
+	<c:if test="${member.level == 0}">
+		일반고객
+	</c:if>
+	<c:if test="${member.level == 1}">
+		관리자
+	</c:if>
+	<!-- for(String s : arr) -->
+	<c:forEach var="str" items="${list}">
+		<div>${str}</div>
+	</c:forEach>
 </body>
 </html>
